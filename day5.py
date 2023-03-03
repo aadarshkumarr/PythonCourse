bc3a8024eccb869f2c0d6bf4dd347f1341ca07cb
AADARSH KUMAR <95774710+aadarshkumarr@users.noreply.github.com>
2023-03-04 01:32:20 +0530

    2nd push

---
+++ day5.py
@@ -0,0 +1,54 @@
+x="""Sinigami
+Loves
+Apples
+"""
+
+# Multiline string
+
+print(x)
+
+# strings are array
+
+str="Death Note"
+
+print(str[6])
+
+print()
+# Loops = calling it self multiple times till the condition get satisfied
+
+for a in str:
+    print(a)
+
+print(len(str)) # to check the length of the sring
+
+print("Note" in str)
+print("Kira" in str)
+
+
+print(str[2:7])
+print(str[:7])
+print(str[2:])
+
+
+print(str[-7:-2])
+
+print(str.upper()) # All Upper case
+print(str.lower()) # all in lower case
+print(str.strip()) # Remove all spaces from starting and end
+
+print(str.replace("D", "d"))
+print(str.split(","))
+
+w="Light"
+e="Yagami"
+
+print(w+e)
+
+print(w+" "+e)
+
+#Google and stude about
+#String Methods
+#Escape Characters
+
+
+
---
+++ day6.py
@@ -0,0 +1,11 @@
+# Conditional Statements
+
+"""
+== Equal to each other
+!= Not equal to each other
+< Smaller then
+> Grater Then
+<= Smaller then Equal to
+>= Greater the equals to
+
+"""
