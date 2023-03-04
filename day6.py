5f45e8a5424f3acd25ae6a41e54ebb0eb0551862
AADARSH KUMAR <95774710+aadarshkumarr@users.noreply.github.com>
2023-03-04 13:22:04 +0530

    Update day6.py

--- day6.py
+++ day6.py
@@ -4,8 +4,31 @@
 == Equal to each other
 != Not equal to each other
 < Smaller then
-> Grater Then
+> Grater then
 <= Smaller then Equal to
 >= Greater the equals to
 
 """
+
+a=343
+b=856
+
+if(a>b):
+    print("a is grater then b")
+
+else:
+    print("b is grater then a")
+
+c = 343
+d = 343
+
+if (c > d):
+    print("a is grater then b")
+
+elif (c < d):
+    print("b is grater then a")
+
+else :
+    print("c is equal to d")
+
+print("a is gratter then b") if a>b else print("b is grater then a")
