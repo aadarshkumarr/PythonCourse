5701e58eaf60a254c56f0a52fb0af1acc502af0c
AADARSH KUMAR <95774710+aadarshkumarr@users.noreply.github.com>
2023-03-04 19:47:15 +0530

    push 3

---
+++ day7.py
@@ -0,0 +1,24 @@
+# Functions
+
+# In python function define by the yeword "def"
+
+def testfunction():
+    print("Hi!!")
+
+# Calling testfunction
+testfunction()
+
+
+# Passing parameter when we define function
+def hellofunction(x):
+    print(x+"world")
+
+# Passing argument when we call a function
+hellofunction("Hello ")
+
+# Passing multiple parameter when we define function
+def mpfunction(x,y):
+    print(x+" "+y)
+
+
+mpfunction("Hello","world")
---
+++ day8.py
@@ -0,0 +1,42 @@
+#  Loops in python
+
+num = [1,2,3,4,5,6,7,89,64,2,34,3,234,6,543,24,43]
+
+for i in num:
+    print(i)
+
+print()
+
+fruit = "apple"
+
+for i in fruit:
+    print(i)
+
+print()
+
+num = [1,2,3,4,5,89,64,543,24,43]
+for i in num:
+    print(i)
+    if i== 5:
+        break
+
+print()
+
+num = [1,2,3,4,5,6,7,89]
+for i in num:
+    if i== 5:
+        continue
+    print(i)
+
+print()
+
+for i in range(14):
+    print(i)
+
+print()
+
+for i in range(5, 14):
+    print(i)
+
+print()
+
